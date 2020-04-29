d3007aaa517ea5f12d28262e449762357b005225
William Wong <compulim@hotmail.com>
2020-04-29 02:04:27 -0700

    Clean up

--- src/utils/promiseRaceMap.ts
+++
@@ -1,9 +0,0 @@
-// /// <reference path="../types/external.d.ts" />
-
-// import entries from 'core-js/features/object/entries';
-
-// export default function promiseRaceMap(map: { [key: string]: Promise<any> }): Promise<any> {
-//   return Promise.race(
-//     entries(map).map(([key, promise]: [string, Promise<any>]) => promise.then(result => ({ [key]: result })))
-//   );
-// }
