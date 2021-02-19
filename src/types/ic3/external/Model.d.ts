2189c964789f7afd36ded56e6e9c43ad0b89ff4b
Charlie Wang <charliewang95@gmail.com>
2021-02-19 14:40:44 -0800

    Uptake registerIC3Error API (#23)
    * commit
    
    * update logs
    
    Co-authored-by: Charlie Wang <charlwan@microsoft.com>

--- src/types/ic3/external/Model.d.ts
+++ src/types/ic3/external/Model.d.ts
@@ -262,6 +262,7 @@ declare namespace Microsoft.CRM.Omnichannel.IC3Client.Model {
     getMessages(): Promise<IMessage[]>;
     registerOnNewMessage(callback: (message: IMessage) => void): Promise<void>;
     registerOnThreadUpdate(callback: (message: IThread) => void): Promise<void>;
+    registerOnIC3Error(callback: (error: any) => void): Promise<void>;
     disconnect(): Promise<void>;
     downloadFile(fileMetadata: IFileMetadata): Promise<Blob>;
     getFileStatus(fileMetadata: IFileMetadata): Promise<FileStatus>;
