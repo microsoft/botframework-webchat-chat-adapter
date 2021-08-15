85f47518cb5fb085447fa523f7990a6f08795099
Erli-ms <55606459+Erli-ms@users.noreply.github.com>
2021-08-14 21:47:10 -0700

    Users/ersuo/trigger get message when poll failed (#36)
    * use conversation from proxy
    
    * tirgger get all messages for missing ack messages
    
    * fix UT

--- src/ic3/Constants.ts
+++ src/ic3/Constants.ts
@@ -1,3 +1,8 @@
 const IC3_CHANNEL_ID = 'ic3';
 
 export { IC3_CHANNEL_ID };
+
+export const MissingAckFromPollingError = 10000;
+export const ReloadAllMessageInterval = 5 * 1000;
+export const Reinitialize = "reinitialize";
+export const Translated = "translated";
